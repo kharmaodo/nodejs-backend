dd98bc52dc30dd5d3483c8f3f27941383c47e005
Maodo DIOP <maodo.diop.external@worldline.com>
2024-12-14 12:07:28 +0000

    feat: Verbes HTTP

--- index.js
+++ index.js
@@ -4,7 +4,26 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('Hello Bambey Master');
+    res.send('GET en cours de mise en oeuvre');
+});
+
+app.get('/:id',(req,res) => {
+const id = req.params.id;
+    res.send(`GET en cours de mise en oeuvre de la ressource ${id}`);
+});
+
+app.post('/',(req,res) => {
+    res.send('POST en cours de mise en oeuvre');
+});
+
+app.put('/:id',(req,res) => {
+    const id = req.params.id;
+    res.json(`PUT en cours de mise en oeuvre de la ressource ${id}`);
+});
+
+app.delete('/:id',(req,res) => {
+    const id = req.params.id;
+   res.json(`DELETE en cours de mise en oeuvre de la ressource ${id}`);
 });
 
 //Lancer l'application avec un numero de port
