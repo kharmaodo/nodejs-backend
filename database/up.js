bec7bb7a1ca482a7f44ba05e16400daa9eefe3f5
Maodo DIOP <dmaodo@gmail.com>
2024-12-28 12:17:40 +0000

    feat: pg

--- database/up.js
+++ database/up.js
@@ -1,77 +1,81 @@
-const { fstat } = require('fs');
-const { pool} = require('../database/db');
+const { pool } = require('../database/db');
 const fs = require('fs');
 const path = require('path');
-const courseFilePath = path.join(__dirname,'cours.json');
-const allCourses = JSON.parse(fs.readFileSync(courseFilePath,'utf-8'));
+const courseFilePath = path.join(__dirname, 'cours.json');
+const allCourses = JSON.parse(fs.readFileSync(courseFilePath, 'utf-8'));
 
 
-const studentFilePath = path.join(__dirname,'student.json');
-const allStudents = JSON.parse(fs.readFileSync(studentFilePath,'utf-8'));
+const studentFilePath = path.join(__dirname, 'student.json');
+const allStudents = JSON.parse(fs.readFileSync(studentFilePath, 'utf-8'));
 
-async function createTableCourseIfNotExits(){
-    const client = await pool.connect();
-            const query = `CREATE TABLE IF NOT EXISTS course (id SERIAL PRIMARY KEY, cours TEXT ,duration TEXT) ` ; 
-            await client.query(query);
+function createTableCourseIfNotExits() {
+    const query = `CREATE TABLE IF NOT EXISTS course (id SERIAL PRIMARY KEY, cours TEXT ,duration TEXT) `;
+    pool.query(query, (err, res) => {
+        if (err) {
+            console.error(err);
+        } else {
             console.log('Table course créée  avec success:');
+        }
+    });
+
 }
 
 
-async function createTableStudentIfNotExits(){
+async function createTableStudentIfNotExits() {
     const client = await pool.connect();
-            const query = `CREATE TABLE IF NOT EXISTS students (id SERIAL PRIMARY KEY, firstName TEXT ,lastName TEXT,email  TEXT,telephone TEXT ) ` ; 
-            await client.query(query);
-            console.log('Table students créée avec success:');
+    const query = `CREATE TABLE IF NOT EXISTS students (id SERIAL PRIMARY KEY, firstName TEXT ,lastName TEXT,email  TEXT,telephone TEXT ) `;
+    await client.query(query);
+    console.log('Table students créée avec success:');
 }
 
-async function insertCourse(){
+async function insertCourse() {
     const client = await pool.connect();
 
     try {
-        for(const currentCourse of allCourses){
-            const { cours, duration }= currentCourse ;
+        for (const currentCourse of allCourses) {
+            const { cours, duration } = currentCourse;
 
-            const query = `INSERT into course(cours,duration) VALUES  ($1,$2) ` ; 
-            const values =[ cours, duration ];
+            const query = `INSERT into course(cours,duration) VALUES  ($1,$2) `;
+            const values = [cours, duration];
 
-            await client.query(query,values);
+            await client.query(query, values);
 
-            console.log('Inserted:',cours);
+            console.log('Inserted:', cours);
         }
     } catch (error) {
         console.error(error);
-        
-    }finally{
+
+    } finally {
         client.release();
     }
 
 }
 
 
-async function insertStudent(){
+async function insertStudent() {
     const client = await pool.connect();
 
     try {
-        for(const currentStudent of allStudents){
-            const { firstName,lastName,email,telephone}= currentStudent ;
+        for (const currentStudent of allStudents) {
+            const { firstName, lastName, email, telephone } = currentStudent;
 
-            const query = `INSERT into students(firstName,lastName,email,telephone) VALUES  ($1,$2,$3,$4) ` ; 
-            const values =[ firstName,lastName,email,telephone ];
+            const query = `INSERT into students(firstName,lastName,email,telephone) VALUES  ($1,$2,$3,$4) `;
+            const values = [firstName, lastName, email, telephone];
 
-            await client.query(query,values);
+            await client.query(query, values);
 
-            console.log('Inserted:',firstName,lastName);
+            console.log('Inserted:', firstName, lastName);
         }
     } catch (error) {
         console.error(error);
-        
-    }finally{
+
+    } finally {
         client.release();
     }
 
 }
-//createTableCourseIfNotExits();
+createTableCourseIfNotExits();
 //insertCourse();
 
-createTableStudentIfNotExits();
-insertStudent();
+//createTableStudentIfNotExits();
+//insertStudent();
