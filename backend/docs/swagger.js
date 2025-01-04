fa5b9a1937a95c5ca221113f1832a227ef8f7f72
Maodo DIOP <maodo.diop.external@worldline.com>
2025-01-04 14:44:50 +0000

    feat: dynamic port

--- backend/docs/swagger.js
+++ backend/docs/swagger.js
@@ -1,15 +1,15 @@
 const swaggerJSDoc = require('swagger-jsdoc');
-
+let port = process.env.PORT || 4000;
 const swaggerDefinition = {
   openapi: '3.0.0',
   info: {
     title: 'Backend for producing Restful API (cours and students)',
     version: '1.0.0',
-    description: '<a target="_blank" href="http://localhost:3000/swagger.json">Documentation</a><br> Produces a Restfull API for course and students Written by Maodo DIOP for academic purpose',
+    description: `<a target="_blank" href="http://localhost:${port}/swagger.json">Documentation</a><br> Produces a Restfull API for course and students Written by Maodo DIOP for academic purpose`,
   },
   servers: [
     {
-      url: 'http://localhost:3000/api',
+      url: `http://localhost:${port}/api`,
       description: 'Serveur local',
     },
   ],
